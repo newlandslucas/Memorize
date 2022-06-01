465fe60ef72c18619dda5853ca5641642080b721
newlandslucas <lucasnewlands.m@gmail.com>
2022-06-01 00:00:28 -0300

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Lucas Machado (Estudos) on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
